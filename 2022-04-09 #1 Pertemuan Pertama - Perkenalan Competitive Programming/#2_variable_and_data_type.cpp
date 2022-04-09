6a9c3ad0878894e5c86612087070e6954ea5ccde
tianreformis <tianreformis@gmail.com>
2022-04-09 14:14:05 +0800

    Tambahkan file latihan.cpp

--- 2022-04-09 #1 Pertemuan Pertama - Perkenalan Competitive Programming/#2_variable_and_data_type.cpp
+++ 2022-04-09 #1 Pertemuan Pertama - Perkenalan Competitive Programming/#2_variable_and_data_type.cpp
@@ -11,9 +11,16 @@ int main() // fungsi utama
     // variabel lokal
     int angka2 = 20;
     int angka1 = 10;
+
+    //var angka1 ubah nilai menjadi 30
+    // cara 1 = angka1=30;
+    // cara 2 = angka1 += 20;
+    // cara 3 = int angka4 = 30; angka1 = angka4;
+   
     float angka3 = 30.20;
+    double angka5 = 20.30;
     printf("Angka 1 = %i, angka 2= %d, Angka 3 = %f\n", angka1,angka2,angka3);
+    printf("Angka 5 = %lf",angka5);
     
-    //var angka1 ubah menjadi 30
     return 0;
 }
---
+++ 2022-04-09 #1 Pertemuan Pertama - Perkenalan Competitive Programming/#3_Latihan.cpp
@@ -0,0 +1,8 @@
+//p1 = 100;
+//p2 = p1;
+//x = 3.1418;
+//y = 234.432;
+
+//Ketentuan
+//p1 dan p2 dalam 1 baris
+//x dan y dalam 1 baris
