59aabc761e850fa8ea53ebc0c05a00d5db468f30
tianreformis <tianreformis@gmail.com>
2022-04-09 13:52:06 +0800

    New Line

---
+++ #2_variable_and_data_type.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdio>
+using namespace std;
+
+// variabel global
+
+
+int main() // fungsi utama
+{
+    // variabel lokal
+    int angka2 = 20;
+    int angka1 = 10;
+    float angka3 = 30.20;
+    printf("Angka 1 = %i, angka 2= %d, Angka 3 = %f\n", angka1,angka2,angka3);
+    
+    return 0;
+}
