7cb78f41b2f96aa5d37573b5916d7df7d5059e8c
tianreformis <tianreformis@gmail.com>
2022-04-09 11:50:33 +0800

    Add New Setting

---
+++ 2022-04-09 #1 Pertemuan Pertama - Perkenalan Competitive Programming/#1_keluaran_dan_masukan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdio>
+
+int main()
+{
+    printf("Hello KSN TIK\n");
+    printf("Hello KSN TIK\n");
+    printf("Hello TIAN\n");
+    return 0;
+}
