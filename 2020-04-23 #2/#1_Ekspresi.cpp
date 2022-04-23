e98a1bde63be76bbcbc0b4c9dae626a99fa5d7c5
tianreformis <tianreformis@gmail.com>
2022-04-23 13:34:54 +0800

    add new file

---
+++ 2020-04-23 #2/#1_Ekspresi.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int hasil = 2 * 3;
+    printf("Variabel tersebut bernilai  = %d\n", hasil);
+    return 0;
+}
--- 2022-04-09 #1 Pertemuan Pertama - Perkenalan Competitive Programming/#4_latihan.cpp
+++ 2022-04-09 #1 Pertemuan Pertama - Perkenalan Competitive Programming/#4_latihan.cpp
@@ -7,3 +7,29 @@ Ukuran Pakaian [S,M,L,XL,XXL) char
 tampilkan variabel pada baris yang terpisah
 
 */
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+int main()
+{
+    string nama;
+    int umur;
+    float tb;
+    char uk;
+
+    cout<<"Masukkan Nama = ";getline (cin, nama);
+    printf("Masukkan Umur = ");scanf("%d",&umur);
+    printf("Masukkan Ukuran Baju = ");scanf("%c",&uk);
+
+    cout<<"Nama Anda = "<<nama<<"\n";
+    printf("Umur Anda = %i\n",umur);
+    printf("Ukuran Baju = %c\n",uk);
+
+
+
+    
+    return 0;
+}
