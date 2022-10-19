d3c6a0b513c2547cceb8cbb2c3727df39b03334d
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2022-10-19 15:32:11 +0800

    add 2023

---
+++ KSN 2023/a+b.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main()
+{
+    int a,b;
+    printf("a,b = ");scanf("%d %d",&a,&b);
+    int hasil = a+b;
+    printf("%d",&hasil);
+}
